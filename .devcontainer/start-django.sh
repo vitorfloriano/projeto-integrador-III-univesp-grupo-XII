487f307cc5f9250e11aba368cfd8bc804ed46498
vitorfloriano <107767584+vitorfloriano@users.noreply.github.com>
2025-04-22 22:30:28 -0300

    Adicionar configuração do ambiente de desenvolvimento com Docker, incluindo Dockerfile, docker-compose e script de inicialização do Django.

---
+++ .devcontainer/start-django.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /workspace/PI_2
+echo "Running Django migrations..."
+python manage.py migrate
+
+echo "Starting Django development server..."
+python manage.py runserver 0.0.0.0:8000
