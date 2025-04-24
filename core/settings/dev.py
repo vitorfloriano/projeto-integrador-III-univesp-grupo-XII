a6138f37fb82991d20627de75d53c65ee99cf2e7
Vitor Floriano <107767584+vitorfloriano@users.noreply.github.com>
2025-04-24 17:25:43 +0000

    Adicionar configuração SECURE_PROXY_SSL_HEADER no ambiente de desenvolvimento

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -7,6 +7,8 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # CSRF settings - adicionar essa configuração
 CSRF_TRUSTED_ORIGINS = ['https://localhost:8000', 'http://localhost:8000', 'http://127.0.0.1:8000']
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # CORS settings for development
 CORS_ALLOW_ALL_ORIGINS = True
 
